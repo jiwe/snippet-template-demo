776dec2cb705977e8e7b6b4988e4311626722c35
jiwe <1106529648@qq.com>
2021-06-21 21:26:41 +0800

    test

---
+++ src/components/Content.js
@@ -0,0 +1,15 @@
+const Content = () => {
+	return (
+		<div>
+			<h2>A fully customizable code snippet editor</h2>
+			<p>Fully customize the color scheme of your code snippet,or use our own premade color schemes. Export your code snippets in JPG,PDF,PNG,or any of the other 10+ common extensions.</p>
+      			<button>Get Started - it's free</button>
+		<div className="bottom">
+      			<img src="/Assets/Logo.svg" />
+			<p>Terms and Conditions Privacy Pocicy</p>
+		</div>
+		</div>	
+	)
+}
+
+export default Content
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,21 @@
+const NavigationBar = () => {
+  return (
+  <div>
+    <div className="head">
+      <img src="/Assets/Logo.svg" />
+      <img src="/Assets/Search Icon.svg" />
+      <button>sign in</button>
+      <button>Get Started - it's free</button>
+    </div>
+    <div>
+	    <h1>Gorgeous Code Snippets</h1>
+	    <p>With Snipper,create fully customizable code snippets in a matter of seconds right from your browseer</p>
+      	    <button>Get Started - it's free</button>
+		<p>no credit card required</p>
+	<img src="/Assets/Snippet Image.png" />
+    </div>
+  </div>
+  )
+}
+
+export default NavigationBar
